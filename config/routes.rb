f9ece81b6c1f018ba6ee62ae5cd0168d348fc7cc
Have Samuel <samiehave9@gmail.com>
2022-07-05 19:31:57 +0300

    Erectified the error

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,5 @@ Rails.application.routes.draw do
   root 'pages#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# gets 'greeting', to: 'custom_pages#hello'
